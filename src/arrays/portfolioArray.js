fb149b063ceb9592a94a40bc29c23022d1d6ed82
MariaLBovin <maria.bovin@gmail.com>
2023-12-16 21:10:39 +0100

    added require to iamges

--- src/arrays/portfolioArray.js
+++ src/arrays/portfolioArray.js
@@ -1,20 +1,20 @@
 const selectedWork = [
     {
-      image: [{ src: '../donutshop.webp', alt: 'en munkshop' }],
+      image: [{ src: require('../donutshop.webp'), alt: 'en munkshop' }],
       title: 'Gottfrids munkar',
       description:
         'Mock-ehandel för en munkshop byggt i Vanilla JS, HTML 5 och SASS.',
       projektlink: 'https://marialbovin.github.io/Gottfrids_munkar/',
     },
     {
-      image: [{ src: '../wheatly2.webp', alt: 'en produktslider' }],
+      image: [{ src:require( '../wheatly2.webp'), alt: 'en produktslider' }],
       title: 'Wheatly',
       description: 'Kodprojekt efter given design i Vue',
       projektlink:
         'https://medieinstitutet.github.io/fed22d-grafiska-verktyg-tomatpureerna/',
     },
     {
-      image: [{ src: '../tic-tac-toe.png', alt: 'ett spel' }],
+      image: [{ src: require('../tic-tac-toe.png'), alt: 'ett spel' }],
       title: 'Tic-tac-toe',
       description: 'Kodprojekt efter given design i Vue',
       projektlink:
@@ -24,7 +24,7 @@ const selectedWork = [
     {
       image: [
         {
-          src: '../arbetsgivardata.png',
+          src: require('../arbetsgivardata.png'),
           alt: 'startsida på en webapplikation till arbetsförmedlingen',
         },
       ],
@@ -38,37 +38,37 @@ const allWork = [
     {
       image: [
         {
-          src: '../clue_1.webp',
+          src: require('../clue_1.webp'),
           alt: 'startsida på ett spel som heter Clue',
         },
       ],
       title: 'Clue',
-      description: 'Ett onlinespel byggt i Vanilla JS, HTML5 och Sass.',
+      description: 'Min första app skriven i Vanilla JS, HTML5 och Sass.',
       projektlink: 'https://marialbovin.github.io/clue/',
     },
     {
-      image: [{ src: '../donutshop_2.webp', alt: 'en munkshop' }],
+      image: [{ src: require('../donutshop_2.webp'), alt: 'en munkshop' }],
       title: 'Gottfrids munkar',
       description:
         'Mock-ehandel för en munkshop byggt i Vanilla JS, HTML 5 och SASS.',
       projektlink: 'https://marialbovin.github.io/Gottfrids_munkar/',
     },
     {
-      image: [{ src: '../wheatly2.webp', alt: 'en produktslider' }],
+      image: [{ src: require('../wheatly2.webp'), alt: 'en produktslider' }],
       title: 'Wheatly',
       description: 'Kodprojekt efter given design i Vue',
       projektlink:
         'https://medieinstitutet.github.io/fed22d-grafiska-verktyg-tomatpureerna/',
     },
     {
-      image: [{ src: '../tic-tac-toe.png', alt: 'ett spel' }],
+      image: [{ src: require('../tic-tac-toe.png'), alt: 'ett spel' }],
       title: 'Tic-tac-toe',
       description: 'Kodprojekt efter given design i Vue',
       projektlink:
         'https://github.com/MariaLBovin/react-tic-tac-toe',
     },
     {
-      image: [{ src: '../zootropolis2.png', alt: 'ett pettingzoo' }],
+      image: [{ src: require('../zootropolis2.png'), alt: 'ett pettingzoo' }],
       title: 'Wheatly',
       description: 'En applikation byggd i react för att hämta, visa och mata djur',
       projektlink:
@@ -77,7 +77,7 @@ const allWork = [
     {
       image: [
         {
-          src: '../arbetsgivardata.png',
+          src: require('../arbetsgivardata.png'),
           alt: 'startsida på en webapplikation till arbetsförmedlingen',
         },
       ],
