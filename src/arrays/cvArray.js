a94e0d61fd4aa2a38e4e484202ac214cf92de7c6
MariaLBovin <maria.bovin@gmail.com>
2025-01-22 14:18:08 +0100

    added to cv

--- src/arrays/cvArray.js
+++ src/arrays/cvArray.js
@@ -1,76 +1,97 @@
 const jobHistory = [
-    {
-        place: 'Norstedts förlagsgrupp',
-        time: '2017-2022',
-        title: 'Kommunikationsansvarig',
-        desc: 'PR - Kommunikationsstrategi - Social mediastrategi - Intern projektledare IT-projekt',
-      },
-      {
-        place: 'Fackförbundet Vision',
-        time: '2017',
-        title: 'Kommunikatör',
-        desc: 'Strategisk kommunikation - Utvecklingsledare utvalda IT-projekt ',
-      },
-      {
-        place: 'Berättarministeriet',
-        time: '2016-2017',
-        title: 'Digitalt ansvarig',
-        desc: 'Intern projektledare IT-projekt - Kommunikationsstrategi sociala medier - Contentproducent sociala medier ',
-      },
-      {
-        place: 'Kungliga Operan',
-        time: '2011-2016',
-        title: 'Kommunikationsansvarig',
-        desc: 'Kommunikationsstrategi - Annonsplanering - Contentproducent sociala medier - KAM ',
-      },
-]
+  {
+    place: "Handelsbanken AB",
+    time: "Feb 2024 - maj 2024",
+    title: "Frontendutvecklare - LIA",
+    desc: "HTML5 - Less - Typescript - React - Jest - Storybook - Azure DevOps - SAFe",
+  },
+  {
+    place: "Sublime AB",
+    time: "Okt 2023 - dec 2023",
+    title: "Frontendutvecklare - LIA",
+    desc: "HTML5 - CSS - Javascript - Umbraco - Optimizely - Azure DevOps - Scrum",
+  },
+  {
+    place: "Norstedts förlagsgrupp",
+    time: "2017-2022",
+    title: "Kommunikationsansvarig",
+    desc: "PR - Kommunikationsstrategi - Social mediastrategi - Intern projektledare IT-projekt",
+  },
+  {
+    place: "Fackförbundet Vision",
+    time: "2017",
+    title: "Kommunikatör",
+    desc: "Strategisk kommunikation - Utvecklingsledare utvalda IT-projekt ",
+  },
+  {
+    place: "Berättarministeriet",
+    time: "2016-2017",
+    title: "Digitalt ansvarig",
+    desc: "Intern projektledare IT-projekt - Kommunikationsstrategi sociala medier - Contentproducent sociala medier ",
+  },
+  {
+    place: "Kungliga Operan",
+    time: "2011-2016",
+    title: "Kommunikationsansvarig",
+    desc: "Kommunikationsstrategi - Annonsplanering - Contentproducent sociala medier - KAM ",
+  },
+];
 
 const eduHistory = [
   {
-  place: 'Medieinstituet',
-  time: '2022-2024',
-  title: 'Frontend developer',
-  desc: 'HTML5 - CSS/SCSS - Vanilla JS - Typescript - Vue - React '
-},
-{
-  place: 'Medieinstituet',
-  time: '2007-2009',
-  title: 'Marknadsstrateg digitala medier',
-  desc: 'Marknadsstrategi - Försäljning - Kommunikationsvetenskap - Beteendevetenskap - Företagsekonomi'
-},
-{
-  place: 'Stockholms universitet',
-  time: '2006-2006',
-  title: 'Engelska A & B',
-  desc: 'Lingvistik - Litteratur - Grammatik'
-},
-]
-
-const skills = [{
-  skills: 'HTLM5',
-  
-},
-{
-  skills: 'CSS',
-  
-},
-{
-  skills: 'SASS',
-  
-},
-{
-  skills: 'Vanilla JS',
-  
-},
-{
-  skills: 'Typescript',
-},
-{
-  skills: 'Vue',
-},
-{
-  skills: 'React',
-},
+    place: "Medieinstituet",
+    time: "2022-2024",
+    title: "Frontend developer",
+    desc: "HTML5 - CSS/SCSS - Javascript - Typescript - Vue - React - Jest - REST - Node.js - Express - SQL - NoSQL - Git ",
+  },
+  {
+    place: "Medieinstituet",
+    time: "2007-2009",
+    title: "Marknadsstrateg digitala medier",
+    desc: "Marknadsstrategi - Försäljning - Kommunikationsvetenskap - Beteendevetenskap - Företagsekonomi",
+  },
+  {
+    place: "Stockholms universitet",
+    time: "2006-2006",
+    title: "Engelska A & B",
+    desc: "Lingvistik - Litteratur - Grammatik",
+  },
 ];
 
-export {jobHistory, eduHistory, skills}
+const skills = [
+  {
+    skills: "HTLM5",
+  },
+  {
+    skills: "CSS/ SCSS / Less",
+  },
+  {
+    skills: "Javascript",
+  },
+  {
+    skills: "Typescript",
+  },
+  {
+    skills: "Jest",
+  },
+  {
+    skills: "Vue",
+  },
+  {
+    skills: "React",
+  },
+  {
+    skills: "Node.js",
+  },
+  {
+    skills: "Express",
+  },
+  {
+    skills: "SQL / NoSQL",
+  },
+  {
+    skills: "Firebase / Firestore",
+  },
+];
+
+export { jobHistory, eduHistory, skills };
